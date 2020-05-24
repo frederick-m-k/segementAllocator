182fd6dc9c55dea5c68327017668c81210b96585
Frederick Kukla <kukla@cip.ifi.lmu.de>
2020-05-24 10:18:09 +0200

    File parser works!

--- src/app/parse-files/parse-files.component.ts
+++ src/app/parse-files/parse-files.component.ts
@@ -78,14 +78,17 @@ export class ParseFilesComponent implements OnInit {
         }
         // ...then the actual data comes through
         if ( lines[first].includes("item []:") ) {
+          console.log("Found the start");
           let startPoint:number;
           let endPoint:number;
           let label:string;
-          for (let tier = 1; tier <= amountOfTiers; tier ++) { // search for tier data as often as amountOfTiers
-            let validLoop:boolean = false;
+          tier: for (let tier = 1; tier <= amountOfTiers; tier ++) { // search for tier data as often as amountOfTiers
+            console.log(lines[first]);
+            console.log(first);
+            let layerOnThisRun:number = 0;
             let borderInformation:number = 0;
             if (lines[first + 1].includes("item [" + tier + "]:")) {
-              for (let second = first; second < lines.length; second ++) {
+              for (let second = first + 2; second < lines.length; second ++) {
                 // TODO what happens when metadata is written in label
                 // Read some metadata first ...
                 if (lines[second].includes("class = ")) {
@@ -97,64 +100,64 @@ export class ParseFilesComponent implements OnInit {
                   if (lines[second].includes(this.firstLayer)) {
                     this.dataStructure.set(this.firstLayer, new Array());
                     this.foundFirst = true;
-                    validLoop = true;
+                    layerOnThisRun = 1;
                   } else if (lines[second].includes(this.secondLayer)) {
                     this.dataStructure.set(this.secondLayer, new Array());
                     this.foundSecond = true;
-                    validLoop = true; 
+                    layerOnThisRun = 2;
                   } else {
-                    continue;
+                    continue tier;
                   }
                 } else if (lines[second].includes("intervals [")) { // Start searching for the actual data
                   watchForNextSegment = true;
-                } else if (lines[second].includes("item[")) {  // Cycle is done
-                  first = first + second;
-                  console.log("Currently on line " + first);
-                  console.log(lines[first]);
+                  borderInformation = 0;
+                } else if (lines[second].includes("item [")) {  // Cycle is done
+                  first = second - 1;
                   break;
                 }
                 // ...and the the actual data
-                if (validLoop) {
-                  if (watchForNextSegment) {
-                    if (!pointTier) {
-                      if (lines[second].includes("xmin =")) {
-                        startPoint = parseFloat(lines[second].split("=")[1].toString().trim());
-                        borderInformation++;
-                        if (isNaN(startPoint) || startPoint == null) { // Reset
-                          watchForNextSegment = false;
-                          borderInformation = 0;
-                          // Write to logfile
-                          console.log(startPoint);
-                        }
-                      } else if (lines[second].includes("xmax =")) {
-                        endPoint = parseFloat(lines[second].split("=")[1].toString().trim());
-                        borderInformation++;
-                        if (isNaN(endPoint) || endPoint == null) {  // Reset
-                          watchForNextSegment = false;
-                          borderInformation = 0;
-                          // Write to logfile
-                          console.log(endPoint);
-                        }
-                      } else if (lines[second].includes("text =")) {
-                        label = lines[second].split("=")[1].toString();
-                        borderInformation++;
-                        if (label == null) {  // Reset
-                          watchForNextSegment = false;
-                          borderInformation = 0;
-                          // Write to logfile
-                          console.log(label);
-                        }
+                if (watchForNextSegment) {
+                  if (!pointTier) {
+                    if (lines[second].includes("xmin =")) {
+                      startPoint = parseFloat(lines[second].split("=")[1].toString().trim());
+                      borderInformation++;
+                      if (isNaN(startPoint) || startPoint == null) { // Reset
+                        watchForNextSegment = false;
+                        borderInformation = 0;
+                        // Write to logfile
+                        console.log("parseFloat didn't work");
+                        console.log(startPoint);
                       }
-                    } else {
-                      // Found a point tier
+                    } else if (lines[second].includes("xmax =")) {
+                      endPoint = parseFloat(lines[second].split("=")[1].toString().trim());
+                      borderInformation++;
+                      if (isNaN(endPoint) || endPoint == null) {  // Reset
+                        watchForNextSegment = false;
+                        borderInformation = 0;
+                        // Write to logfile
+                        console.log("parseFloat didn't work");
+                        console.log(endPoint);
+                      }
+                    } else if (lines[second].includes("text =")) {
+                      label = lines[second].split("=")[1].toString();
+                      borderInformation++;
+                      if (label == null) {  // Reset
+                        watchForNextSegment = false;
+                        borderInformation = 0;
+                        // Write to logfile
+                        console.log(lines[second] + " should contain the label");
+                        console.log(label);
+                      }
                     }
-                    if (borderInformation == 3) {
-                      watchForNextSegment = false;
-                      if (this.foundFirst) {
-                        this.dataStructure.get(this.firstLayer).push([startPoint, endPoint, label]);
-                      } else if (this.foundSecond) {
-                        this.dataStructure.get(this.secondLayer).push([startPoint, endPoint, label]);
-                      }
+                  } else {
+                    // Found a point tier
+                  }
+                  if (borderInformation == 3) {
+                    watchForNextSegment = false;
+                    if (layerOnThisRun === 1) {
+                      this.dataStructure.get(this.firstLayer).push([startPoint, endPoint, label]);
+                    } else if (layerOnThisRun === 2) {
+                      this.dataStructure.get(this.secondLayer).push([startPoint, endPoint, label]);
                     }
                   }
                 }
